e3c692cf58fdc2999d4b999636dfca90ad96bdeb
maydrvll <mayeul_90@hotmail.com>
2014-06-20 18:43:23 +0200

    scss, page crea, fin journee 20

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 
 	<!-- Latest compiled and minified JavaScript -->
-	<link rel="stylesheet" href="style.css" type"text/css">
+	<link rel="stylesheet" href="css/style.css" type"text/css">
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
